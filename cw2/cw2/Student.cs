7fe6f58f2970a6652e06fa41f7cc92c5dae0880d
Paweł Pietrzak <s18793@pjwstk.edu.pl>
2020-03-22 18:27:44 +0100

    add Student

--- cw2/cw2/Student.cs
+++ cw2/cw2/Student.cs
@@ -7,5 +7,43 @@ namespace cw2
 {
     public class Student
     {
+        public String Id
+        {
+            get; set; 
+        }
+
+        public String Name
+        {
+            get; set;
+        }
+        public String LastName
+        {
+            get; set;
+        }
+        public String Bdate
+        {
+            get; set;
+        }
+        public String Email
+        {
+            get; set;
+        }
+        public String Studies_Name
+        {
+            get; set;
+        }
+        public String Studies_mode
+        {
+            get; set;
+        }
+        public String Mothers_Name
+        {
+            get; set;
+        }
+        public String Fathers_Name
+        {
+            get; set;
+        }
+
     }
-}
+
