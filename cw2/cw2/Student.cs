e3c2ec3dd48bb23bced0842edb9fba3ded18ea18
Paweł Pietrzak <s18793@pjwstk.edu.pl>
2020-03-22 19:30:04 +0100

    XmlElement student

--- cw2/cw2/Student.cs
+++ cw2/cw2/Student.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.Json.Serialization;
 using System.Xml.Serialization;
 
 namespace cw2
@@ -6,37 +7,52 @@ namespace cw2
     public class Student 
     {
 
-        [XmlAttribute]
+        [XmlElement(ElementName = "indexNumber")]
+        [JsonPropertyName("indexNumber")]
         public String Id
         {
             get; set;
         }
+        [XmlElement(ElementName = "fname")]
+        [JsonPropertyName("fname")]
 
         public String Name
         {
             get; set;
         }
+        [XmlElement(ElementName = "lname")]
+        [JsonPropertyName("lname")]
         public String LastName
         {
             get; set;
         }
+        [XmlElement(ElementName = "birthdate")]
+        [JsonPropertyName("birthdate")]
         public String Bdate
         {
             get; set;
         }
+        [XmlElement(ElementName = "email")]
+        [JsonPropertyName("email")]
         public String Email
         {
             get; set;
         }
+        [XmlElement(ElementName = "studies")]
+        [JsonPropertyName("studies")]
         public Studies Studies
          {  
             get; set;
 
          }
+        [XmlElement(ElementName = "mothersName")]
+        [JsonPropertyName("mothersName")]
         public String Mothers_Name
         {
             get; set;
         }
+        [XmlElement(ElementName = "fathersName")]
+        [JsonPropertyName("fathersName")]
         public String Fathers_Name
         {
             get; set;
